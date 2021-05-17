20a89c92dbae1b948253c2142adead2bc1b643b5
zhangjian <zhangxjian@me.com>
2021-05-17 11:26:37 +0800

    update day05

---
+++ Dayof5/Dayof5/RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  Dayof5
+//
+//  Created by 张剑 on 2021/5/17.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+  
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var locationLabel: UILabel!
+  @IBOutlet var typeLabel: UILabel!
+  @IBOutlet var thumbnailImageView: UIImageView! {
+    didSet {
+      thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+      thumbnailImageView.clipsToBounds = true
+    }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+}
