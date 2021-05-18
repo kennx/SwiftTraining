ce22b8a8dfc842aeff0e7c679ff7f0a83ef83f83
zhangjian <zhangxjian@me.com>
2021-05-18 14:43:57 +0800

    update

--- Dayof5/Dayof5/RestaurantTableViewController.swift
+++ Dayof5/Dayof5/RestaurantTableViewController.swift
@@ -13,19 +13,19 @@ class RestaurantTableViewController: UITableViewController {
   var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
   
   var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-
+  
   var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
   
   var restaurantIsVisited = Array(repeating: false, count: 21)
-  
+    
   
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.cellLayoutMarginsFollowReadableWidth = true
   }
-
-
-
+  
+  
+  
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
@@ -33,7 +33,7 @@ class RestaurantTableViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return restaurantNames.count
   }
-
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cellIdentifier = "datacell"
     let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
@@ -44,7 +44,7 @@ class RestaurantTableViewController: UITableViewController {
     cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
     
     cell.heartTick.isHidden = !restaurantIsVisited[indexPath.row]
-
+    
     return cell
   }
   
@@ -71,21 +71,89 @@ class RestaurantTableViewController: UITableViewController {
     
     
     
-    if let cell = tableView.cellForRow(at: indexPath) {
-      let title = self.restaurantIsVisited[indexPath.row] == true ? "Undo Check-in" : "Check in"
-      let checkInAction = UIAlertAction(title: title, style: .default, handler: { (action: UIAlertAction!) -> Void in
-        let restaurantCell = cell as! RestaurantTableViewCell
-        restaurantCell.heartTick.isHidden = self.restaurantIsVisited[indexPath.row]
-        
-        self.restaurantIsVisited[indexPath.row] = self.restaurantIsVisited[indexPath.row] ? false : true
-      })
-      optionMenu.addAction(checkInAction)
-    }
+    let title = self.restaurantIsVisited[indexPath.row] ? "Undo Check-in" : "Check in"
+    
+    let checkInAction = UIAlertAction(title: title, style: .default, handler: { (action: UIAlertAction!) -> Void in
+      let cell = tableView.cellForRow(at: indexPath) as! RestaurantTableViewCell
+      cell.heartTick.isHidden = self.restaurantIsVisited[indexPath.row]
+      self.restaurantIsVisited[indexPath.row] = self.restaurantIsVisited[indexPath.row] ? false : true
+    })
+    optionMenu.addAction(checkInAction)
     
     
     present(optionMenu, animated: true, completion: nil)
     
     tableView.deselectRow(at: indexPath, animated: false)
+  }
+  
+  override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    
+    
+    let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+      self.restaurantNames.remove(at: indexPath.row)
+      self.restaurantLocations.remove(at: indexPath.row)
+      self.restaurantTypes.remove(at: indexPath.row)
+      self.restaurantIsVisited.remove(at: indexPath.row)
+      self.restaurantImages.remove(at: indexPath.row)
+      
+      self.tableView.deleteRows(at: [indexPath], with: .fade)
+      
+      completionHandler(true)
+    }
+    
+    let shareAction = UIContextualAction(style: .normal, title: "Share") { (action, sourceView, completionHandler) in
+      let defaultText = "Just checking in at \(self.restaurantNames[indexPath.row])"
+      let activityController: UIActivityViewController
+      if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+        activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+      } else {
+        activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+      }
+      
+      if let popoverController = activityController.popoverPresentationController {
+        if let cell = tableView.cellForRow(at: indexPath) {
+          popoverController.sourceView = cell
+          popoverController.sourceRect = cell.bounds
+        }
+      }
+      
+      self.present(activityController, animated: true, completion: nil)
+      completionHandler(true)
+    }
+    
+    deleteAction.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+    deleteAction.image = UIImage(systemName: "trash")
+    shareAction.backgroundColor = UIColor(red: 254.0/255.0, green: 149.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+    shareAction.image = UIImage(systemName: "square.and.arrow.up")
+    
+    let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+    
+    
+    
+    return swipeConfiguration
+  }
+  
+  override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    
+    
+    let cell = tableView.cellForRow(at: indexPath) as! RestaurantTableViewCell
+    
+    let tickAction = UIContextualAction(style: .normal, title: "tick") { (action, sourceView, completionHandler) in
+      
+      self.restaurantIsVisited[indexPath.row].toggle()
+      cell.heartTick.isHidden = !self.restaurantIsVisited[indexPath.row]
+      completionHandler(true)
+    }
+    
+    tickAction.backgroundColor = UIColor(red: 79.0/255.0, green: 157.0/255.0, blue: 86.0/255.0, alpha: 1.0)
+    tickAction.image = self.restaurantIsVisited[indexPath.row] ? UIImage(named: "undo") : UIImage(named: "tick")
+    
+    let swipeConfiguration = UISwipeActionsConfiguration(actions: [tickAction])
+    
+    tableView.deselectRow(at: indexPath, animated: false)
+    
+    return swipeConfiguration
+    
   }
   
 }
